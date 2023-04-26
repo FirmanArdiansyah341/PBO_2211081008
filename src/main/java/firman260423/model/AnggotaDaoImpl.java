896035c174a54f1059a03ac8e789dc5be10687b3
FirmanArdiansyah <ardiansyahfirman2004@gmail.com>
2023-04-26 11:43:19 +0700

    26-04-2023

--- src/main/java/firman260423/model/AnggotaDaoImpl.java
+++ src/main/java/firman260423/model/AnggotaDaoImpl.java
@@ -14,23 +14,28 @@ import java.util.List;
 public class AnggotaDaoImpl implements AnggotaDao {
     private List<Anggota> data = new ArrayList<>();
     
+    @Override
     public Anggota save(Anggota anggota){
         data.add(anggota);
         return anggota;
     }
     
+    @Override
     public Anggota update (int index, Anggota anggota){
         data.set(index, anggota);
         return anggota;
     }
     
+    @Override
     public void delete (int index){
         data.remove(index);
     }
+    @Override
     public Anggota getAnggota(int index){
         return data.get(index);
     }
     
+    @Override
     public List<Anggota> getAllAnggota(){
         return data;
     }
